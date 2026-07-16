42afc338725a203617f4df3adb77d377ef14a66e
kannankdl35 <145252426+kannankdl35@users.noreply.github.com>
2026-07-16 21:27:08 +0530

    Initialize bot with Pyrogram client

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
 
+app = Client(
+    "CMGMovieRecommendationBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+if __name__ == "__main__":
+    print("Bot is starting...")
+    app.run()
