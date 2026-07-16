7edf7a9fa23c2d812a5769d64e4a3d05dd317652
kannankdl35 <145252426+kannankdl35@users.noreply.github.com>
2026-07-17 01:49:40 +0530

    Refactor bot.py to use plugins dictionary

--- bot.py
+++ bot.py
@@ -2,18 +2,12 @@ from pyrogram import Client
 
 from config import API_ID, API_HASH, BOT_TOKEN
 
-# Import Plugins
-import plugins.start
-import plugins.callback
-import plugins.movie
-import plugins.series
-import plugins.details
-
 app = Client(
     "CMGMovieRecommendationBot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
+    plugins=dict(root="plugins")
 )
 
 print("✅ CMG Movie Recommendation Bot Started...")
