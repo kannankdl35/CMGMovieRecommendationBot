d3adb27df8f1244a0b866cdb2f8615203be25539
kannankdl35 <145252426+kannankdl35@users.noreply.github.com>
2026-07-17 00:27:24 +0530

    Add TMDb genre IDs for movies and TV shows
    Defines movie and TV genre mappings based on TMDb Genre IDs.

---
+++ database/genres.py
@@ -0,0 +1,45 @@
+"""
+TMDb Genre IDs
+https://developer.themoviedb.org/docs/genres
+"""
+
+MOVIE_GENRES = {
+    "action": 28,
+    "adventure": 12,
+    "animation": 16,
+    "comedy": 35,
+    "crime": 80,
+    "documentary": 99,
+    "drama": 18,
+    "family": 10751,
+    "fantasy": 14,
+    "history": 36,
+    "horror": 27,
+    "music": 10402,
+    "mystery": 9648,
+    "romance": 10749,
+    "science_fiction": 878,
+    "tv_movie": 10770,
+    "thriller": 53,
+    "war": 10752,
+    "western": 37,
+}
+
+TV_GENRES = {
+    "action_adventure": 10759,
+    "animation": 16,
+    "comedy": 35,
+    "crime": 80,
+    "documentary": 99,
+    "drama": 18,
+    "family": 10751,
+    "kids": 10762,
+    "mystery": 9648,
+    "news": 10763,
+    "reality": 10764,
+    "science_fiction_fantasy": 10765,
+    "soap": 10766,
+    "talk": 10767,
+    "war_politics": 10768,
+    "western": 37,
+}
