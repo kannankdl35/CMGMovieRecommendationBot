3091eb75a6506ae257adc4c377b66dba48a585e8
kannankdl35 <145252426+kannankdl35@users.noreply.github.com>
2026-07-16 21:28:10 +0530

    Add environment variables for Telegram and TMDb APIs

--- config.py
+++ config.py
@@ -3,10 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Telegram
 API_ID = int(os.getenv("API_ID", 0))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+# TMDb API
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+
+# MongoDB
 MONGO_URI = os.getenv("MONGO_URI")
-MOVIE_BOT_USERNAME = os.getenv("MOVIE_BOT_USERNAME")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "CMGMovieRecommendationBot")
