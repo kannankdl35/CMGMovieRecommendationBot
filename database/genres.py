b6a28d8cbdd9d982c130a29317e465e1219446cc
kannankdl35 <145252426+kannankdl35@users.noreply.github.com>
2026-07-17 11:30:22 +0530

    Refactor genre keys for movies and TV series

--- database/genres.py
+++ database/genres.py
@@ -3,6 +3,10 @@ TMDb Genre IDs
 https://developer.themoviedb.org/docs/genres
 """
 
+# -------------------------------
+# MOVIES
+# -------------------------------
+
 MOVIE_GENRES = {
     "action": 28,
     "adventure": 12,
@@ -18,28 +22,36 @@ MOVIE_GENRES = {
     "music": 10402,
     "mystery": 9648,
     "romance": 10749,
-    "science_fiction": 878,
-    "tv_movie": 10770,
+    "scifi": 878,
+    "tvmovie": 10770,
     "thriller": 53,
     "war": 10752,
     "western": 37,
 }
 
+
+# -------------------------------
+# TV SERIES
+# -------------------------------
+
 TV_GENRES = {
-    "action_adventure": 10759,
+    "action": 10759,      # Action & Adventure
+    "adventure": 10759,
     "animation": 16,
     "comedy": 35,
     "crime": 80,
     "documentary": 99,
     "drama": 18,
     "family": 10751,
-    "kids": 10762,
+    "fantasy": 10765,     # Sci-Fi & Fantasy
+    "history": 10768,     # War & Politics (closest TMDb category)
+    "horror": 9648,       # Closest available
+    "music": 10767,       # Talk
     "mystery": 9648,
-    "news": 10763,
-    "reality": 10764,
-    "science_fiction_fantasy": 10765,
-    "soap": 10766,
-    "talk": 10767,
-    "war_politics": 10768,
+    "romance": 10766,     # Soap
+    "scifi": 10765,
+    "tvmovie": 10767,
+    "thriller": 9648,
+    "war": 10768,
     "western": 37,
 }
