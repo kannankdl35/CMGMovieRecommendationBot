3fac925f27d3337894ac230fa6ba22941cf11284
kannankdl35 <145252426+kannankdl35@users.noreply.github.com>
2026-07-17 11:53:23 +0530

    Implement user results management functions
    Added functions to save, retrieve, and clear user recommendation results.

--- database/user_state.py
+++ database/user_state.py
@@ -1,5 +1,5 @@
 # Temporary in-memory user state
-# Later we'll move this to MongoDB
+# Later can be moved to MongoDB
 
 user_states = {}
 
@@ -17,3 +17,21 @@ def get_state(user_id: int):
 
 def clear_state(user_id: int):
     user_states.pop(user_id, None)
+
+
+# ---------- Recommendation Results ----------
+
+def save_results(user_id: int, results):
+    if user_id not in user_states:
+        user_states[user_id] = {}
+
+    user_states[user_id]["results"] = results
+
+
+def get_results(user_id: int):
+    return user_states.get(user_id, {}).get("results", [])
+
+
+def clear_results(user_id: int):
+    if user_id in user_states:
+        user_states[user_id].pop("results", None)
