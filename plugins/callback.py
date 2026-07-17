b7fed242c6157520d8bcbfed4abe153a25615f98
kannankdl35 <145252426+kannankdl35@users.noreply.github.com>
2026-07-17 09:09:27 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -3,24 +3,41 @@ from pyrogram.types import CallbackQuery
 
 from keyboards.type import type_keyboard
 from keyboards.home import home_keyboard
+from keyboards.genre import genre_keyboard
 
 
 @Client.on_callback_query()
 async def callback_handler(client: Client, callback: CallbackQuery):
 
-    if callback.data == "suggest_me":
+    data = callback.data
+
+    if data == "suggest_me":
 
         await callback.message.edit_text(
             text="🎬 **Select what you're looking for**",
             reply_markup=type_keyboard()
         )
 
-    elif callback.data == "back_home":
+    elif data == "movies":
+
+        await callback.message.edit_text(
+            text="🎬 **Select a Movie Genre**",
+            reply_markup=genre_keyboard("movie")
+        )
+
+    elif data == "series":
+
+        await callback.message.edit_text(
+            text="📺 **Select a Series Genre**",
+            reply_markup=genre_keyboard("series")
+        )
+
+    elif data == "back_home":
 
         await callback.message.edit_text(
             text=(
                 "👋 **Welcome to CMG Movie Recommendation Bot**\n\n"
-                "🎬 Discover Movies & TV Series based on:\n"
+                "🎬 Discover Movies & TV Series based on:\n\n"
                 "• 🎭 Genre\n"
                 "• 🌍 Language\n"
                 "• ⭐ Rating\n\n"
