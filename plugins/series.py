2636a66963b1cd12f8e0f52d6367a8e3b8d8e956
kannankdl35 <145252426+kannankdl35@users.noreply.github.com>
2026-07-17 09:57:47 +0530

    Update series.py

--- plugins/series.py
+++ plugins/series.py
@@ -1,17 +1,2 @@
-from pyrogram import Client
-from pyrogram.types import CallbackQuery
-
-from keyboards.genre import genre_keyboard
-
-
-@Client.on_callback_query()
-async def series_callback(client, callback: CallbackQuery):
-
-    if callback.data == "series":
-
-        await callback.message.edit_text(
-            text="📺 **Select Series Genre**",
-            reply_markup=genre_keyboard("series")
-        )
-
-        await callback.answer()
+# Series callbacks are temporarily disabled.
+# They will be merged into callback.py later.
